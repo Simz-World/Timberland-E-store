81f4e2194f4e33ccd62eb510d753fc1201786624
MR Snyman <Snyman.s18@gmail.com>
2023-10-06 19:20:08 +0200

    second commit

---
+++ app.js
@@ -0,0 +1,4 @@
+const wrapper = document.querySelector(".slidewrapper")
+const menuItems = document.querySelectorAll(".menuItem")
+
+menuItems.forEach((item,index)=>{item.addEventListener("click")})
